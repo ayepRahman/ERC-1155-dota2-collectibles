8bd272c5643224e6e17041b7c814be343342f5fa
arifrahman <arif@indorse.io>
2019-04-04 00:50:45 +0800

    updated

--- contracts/Dota2Collectibles.sol
+++ contracts/Dota2Collectibles.sol
@@ -2,6 +2,27 @@ pragma solidity >=0.5.0 <0.6.0;
 
 import 'multi-token-standard/contracts/tokens/ERC1155/ERC1155.sol';
 
+// we can do multiple extend with a coma is ERC1155, ERC1155TokenReceiver
 contract Dota2Collectibles is ERC1155 {
+  address public owner;
+
+  // TODO:
+  // create item
+  // create mint item to create multiple id of a same token?
+  // getAllItems
+  // getSingleItem by wallet address
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function createItem() public onlyOwner {
+
+  }
+
+  function mintItems() public {
+
+  }
 
 }
